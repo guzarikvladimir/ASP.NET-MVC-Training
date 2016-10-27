f45a40176c9652283b743222e34c3d875d724d2f
guzarikvladimir <guzarikvladimir@gmail.com>
2016-10-27 16:18:04 +0300

    Тесты к третьемузаданию второго дня

---
+++ NET.W.2016.01.Guzarik.04/Task3.Tests/BitsNUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Tests
+{
+    [TestFixture]
+    class BitsNUnitTests
+    {
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(0, 15, 30, 30, ExpectedResult = 1073741824)]
+        [TestCase(0, 15, 0, 30, ExpectedResult = 15)]
+        [TestCase(15, -15, 0, 4, ExpectedResult = 31)]
+        [TestCase(15, int.MaxValue, 3, 5, ExpectedResult = 63)]
+        public int Insertion_NormalBehaviur_ChangedNumber(int a, int b, int i, int j)
+        {
+            return Bits.Insertion(a, b, i, j);
+        }
+    }
+}
---
+++ NET.W.2016.01.Guzarik.04/Task3.Tests/BitsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task3.Tests
+{
+    [TestClass]
+    public class BitsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Insertion_iBiggerThanj_ArgumentException()
+        {
+            Bits.Insertion(8, 15, 30, 0);
+        }
+    }
+
+}
