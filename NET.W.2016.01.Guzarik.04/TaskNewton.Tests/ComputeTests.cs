7d38e96049d6462199de6560ef3fdf9b834bcbc2
guzarikvladimir <guzarikvladimir@gmail.com>
2016-10-27 16:21:45 +0300

    Тесты к третьему заданию

---
+++ NET.W.2016.01.Guzarik.04/TaskNewton.Tests/ComputeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskNewton.Tests
+{
+    [TestClass]
+    public class ComputeTests
+    {
+        [TestMethod]
+        public void Sqrt_PositiveIntNumer81WhereNIs4_3()
+        {
+            double eps = 0.001;
+            double actual = Compute.Sqrt(81, 4, eps);
+
+            Assert.IsTrue(actual >= 3 - eps && actual <= 3 + eps);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Sqrt_NegativeNumber_ArgumentException()
+        {
+            Compute.Sqrt(-1);
+        }
+    }
+}
