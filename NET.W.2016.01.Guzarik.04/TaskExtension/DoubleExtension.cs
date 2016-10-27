3560b3a410e7301038cdbb630d9e84b57c376059
guzarikvladimir <guzarikvladimir@gmail.com>
2016-10-27 16:18:32 +0300

    Третье задание

---
+++ NET.W.2016.01.Guzarik.04/TaskExtension/DoubleExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExtension
+{
+    /// <summary>
+    /// Класс, содержащий методы расширения для чисел Double
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// Метод для получения двоичного представления числа
+        /// </summary>
+        /// <param name="seed"></param>
+        /// <returns>Битовое представление числа, типа BitArray</returns>
+        public static BitArray GetBits(this double seed)
+        {
+            BitArray bits = new BitArray(BitConverter.GetBytes(seed));
+            
+            return bits;
+        }
+    }
+}
