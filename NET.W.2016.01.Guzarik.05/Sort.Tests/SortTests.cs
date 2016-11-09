6dc72b2a954b727af030b135fbe92c6dcd2b168d
guzarikvladimir <guzarikvladimir@gmail.com>
2016-11-09 23:10:59 +0300

    Рефакторинг методов сравнения и зарождение метода сортировки, принимающий делегат

--- NET.W.2016.01.Guzarik.05/Sort.Tests/SortTests.cs
+++ NET.W.2016.01.Guzarik.05/Sort.Tests/SortTests.cs
@@ -1,17 +1,12 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Sort.Tests
 {
     [TestFixture]
     public class SortTests
     {
-        [Test, TestCaseSource("NormalCases")]
+        [Test, TestCaseSource(nameof(NormalCases))]
         public void Bubble_NormalCases(int[][] actual, int[][] expected, IComparer<int[]> comp)
         {
             //Sort.Bubble(actual, comp);
@@ -28,25 +23,24 @@ namespace Sort.Tests
             return min2 - min1;
         }
 
-        static object[] NormalCases = new object[]
-        {
-            new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 } },
-                new int[][] { new int[] { 1, 4 }, new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 } },
+        private static readonly object[] NormalCases = {
+            new object[] { new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 }, new[] { 1, 4 } },
+                new[] { new[] { 1, 4 }, new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 } },
                 new OnSumUp() },
-            new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 } },
-                new int[][] { new int[] { 2, 3, 4, 5 }, new int[] { 1, 2, 3 }, new int[] { 1, 4 } },
+            new object[] { new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 }, new[] { 1, 4 } },
+                new[] { new[] { 2, 3, 4, 5 }, new[] { 1, 2, 3 }, new[] { 1, 4 } },
                 new OnSumDown() },
-            new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 } },
-                new int[][] { new int[] { 1, 2, 3 }, new int[] { 1, 4 }, new int[] { 2, 3, 4 , 5 } },
+            new object[] { new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 }, new[] { 1, 4 } },
+                new[] { new[] { 1, 2, 3 }, new[] { 1, 4 }, new[] { 2, 3, 4 , 5 } },
                 new OnMaxUp() },
-            new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 } },
-                new int[][] { new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 }, new int[] { 1, 2, 3 } },
+            new object[] { new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 }, new[] { 1, 4 } },
+                new[] { new[] { 2, 3, 4, 5 }, new[] { 1, 4 }, new[] { 1, 2, 3 } },
                 new OnMaxDown() },
-            new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 } },
-                new int[][] { new int[] { 1, 2, 3 }, new int[] { 1, 4 }, new int[] { 2, 3, 4, 5 } },
+            new object[] { new [] { new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 }, new[] { 1, 4 } },
+                new[] { new[] { 1, 2, 3 }, new[] { 1, 4 }, new[] { 2, 3, 4, 5 } },
                 new OnMinUp() },
-            new object[] { new int[][] { new int[] { 1, 2, 3 }, new int[] { 2, 3, 4, 5 }, new int[] { 1, 4 } },
-                new int[][] { new int[] { 2, 3, 4, 5 }, new int[] { 1, 2, 3 }, new int[] { 1, 4 } },
+            new object[] { new[] { new[] { 1, 2, 3 }, new[] { 2, 3, 4, 5 }, new[] { 1, 4 } },
+                new[] { new[] { 2, 3, 4, 5 }, new[] { 1, 2, 3 }, new[] { 1, 4 } },
                 new OnMinDown() }
         };
     }
