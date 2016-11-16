8c38e3b262e4897446eaafbbd30df88261079408
guzarikvladimir <guzarikvladimir@gmail.com>
2016-11-16 11:31:06 +0300

    Числа Фибоначчи

---
+++ NET.W.2016.01.Guzarik.11/Task1/Generate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Task1
+{
+    /// <summary>
+    /// Class for generating different sequences
+    /// </summary>
+    public static class Generate
+    {
+        /// <summary>
+        /// Generating the sequences of Fibonacci numbers
+        /// </summary>
+        public static IEnumerable<int> Fibonacci(int count)
+        {
+            int i = 0;
+            int prev = 0, current = 1;
+
+            do
+            {
+                yield return current;
+
+                int tmp = current;
+                current = prev + tmp;
+                prev = tmp;
+            } while (++i < count);
+        }
+    }
+}
