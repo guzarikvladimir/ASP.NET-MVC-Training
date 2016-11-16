166e594b99f4f7779786ae47b6cd20b477b99b2e
guzarikvladimir <guzarikvladimir@gmail.com>
2016-11-16 11:31:17 +0300

    Числа Фибоначчи

---
+++ NET.W.2016.01.Guzarik.11/Task1.Tests/GenerateTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class GenerateTests
+    {
+        [TestCase(2, new[] {1, 1})]
+        [TestCase(10, new[] {1, 1, 2, 3, 5, 8, 13, 21, 34, 55})]
+        public void Fibonacci_ValidSmallNumbers(int number, int[] expected)
+        {
+            var actual = Generate.Fibonacci(number).ToArray();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
